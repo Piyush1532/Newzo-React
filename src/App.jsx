49f0b0a3426eae6273abd3f64b8fbce4e599a5f8
Piyush1532 <piyushadav503@gmail.com>
2024-07-31 13:16:23 +0530

    added store provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Newsboard from './Components/NewsBoard/Newsboard'
+import { Provider } from 'react-redux'
+
 
 function App() {
   const [categories, setCategories] = useState("home")
   return (
     <>
 <Navbar setCategories={setCategories} categories={categories}/>
+<Newsboard/>
+
+
     </>
   )
 }
