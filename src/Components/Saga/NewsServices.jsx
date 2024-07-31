5ece76ab446ad3e4c8b1b606075c2b656b8e4d50
Piyush1532 <piyushadav503@gmail.com>
2024-07-31 13:19:03 +0530

    fixed error

--- src/Components/Saga/NewsServices.jsx
+++ src/Components/Saga/NewsServices.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const GetLatestNewsServices = async () => {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=51663eacf9ad40e4951abc9e41bbf44e`);
-    console.log(response.data)
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=51663eacf9ad40e4951abc9e41bbf44e`);
+    console.log(response.data.articles)
   return response.data.articles;
 
   };
   
+
+  console.log("hello world piyush")
